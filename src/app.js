6da650cd893edb6992b93d3424fdcb5f6931eac8
Felipe Garcia <felipesgarcia17@gmail.com>
2026-03-21 14:17:27 -0400

    Register auth routes in app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const usersRoutes = require('./routes/users');
 const resourcesRoutes = require('./routes/resources');
 const reservationsRoutes = require('./routes/reservations');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/resources', resourcesRoutes);
 app.use('/api/reservations', reservationsRoutes);
