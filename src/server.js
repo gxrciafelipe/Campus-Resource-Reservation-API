c20efc30e5aa1ae479e9e9b3ecd344497ac99aa3
Felipe Garcia <felipesgarcia17@gmail.com>
2026-05-06 04:34:56 -0400

    use config.PORT in server instead of hardcoded value

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
- 
-const PORT = 3000;
- 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const config = require('./config');
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
